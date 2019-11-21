1f1d2a9a8e40d63965ecc9b347552c791b103b99
Abigail Wiggins <rebtaylor14@gmail.com>
2019-11-21 11:43:25 -0600

    Refactoring of tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,84 +1,76 @@
 package test;
 
 import main.BalancedBrackets;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    BalancedBrackets test_class;
-
-    @Before
-    public void setup() {
-        test_class = new BalancedBrackets();
-    }
-
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertTrue(true);
     }
 
     @Test
     public void onlyBracketsReturnsTrue() {
-        assertTrue(test_class.hasBalancedBrackets("[]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
     @Test
     public void emptyStringReturnsTrue() {
-        assertTrue(test_class.hasBalancedBrackets(""));
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
 
     @Test
     public void whitespaceReturnsTrue() {
-        assertTrue(test_class.hasBalancedBrackets(" "));
+        assertTrue(BalancedBrackets.hasBalancedBrackets(" "));
     }
 
     @Test
     public void onlyLeftBracketReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
     }
 
     @Test
     public void onlyRightBracketReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
     }
 
     @Test
     public void backwardsBracketsReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("]["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
     }
 
     @Test
     public void balancedBracketsWithLettersReturnsTrue() {
-        assertTrue(test_class.hasBalancedBrackets("[Abigail]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Abigail]"));
     }
 
     @Test
     public void leftBracketWithLettersReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("[Abigail"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Abigail"));
     }
 
     @Test
     public void rightBracketWithLettersReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("Abigail]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Abigail]"));
     }
 
     @Test
     public void backwardsBracketsWithLettersReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("]Abigail["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Abigail["));
     }
 
     @Test
     public void backwardsBracketsWithLettersOutsideReturnsFalse() {
-        assertFalse(test_class.hasBalancedBrackets("Ab]ig[ail"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Ab]ig[ail"));
     }
 
     @Test
     public void bracketsWithLettersOutsideReturnsTrue() {
-        assertTrue(test_class.hasBalancedBrackets("A[big]ail"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("A[big]ail"));
     }
 
 }
